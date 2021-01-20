b007e37aa136b5dc6ce6e591ec78e5929b670f5f
Maciej Bak <wsciekly.maciek@gmail.com>
2021-01-20 21:15:26 +0100

    <<

--- hypercomplex/Hypercomplex.hpp
+++ hypercomplex/Hypercomplex.hpp
@@ -758,11 +758,11 @@ std::ostream& operator<<(
     char* outstr;
     for (unsigned int i=0; i < dim - 1; i++) {
         outstr = mpfr_get_str(NULL, exponent, 10, 0, H[i], MPFR_RNDN);
-        os << outstr << " ";
+        os << outstr << "E" << " ";
         mpfr_free_str(outstr);
     }
     outstr = mpfr_get_str(NULL, exponent, 10, 0, H[dim - 1], MPFR_RNDN);
-    os << outstr;
+    os << outstr << "E";
     mpfr_free_str(outstr);
     return os;
 }
