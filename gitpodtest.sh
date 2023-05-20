52e26dcc5b130b65d8576b7633c9215c35cae3b3
Maciej Bak <wsciekly.maciek@gmail.com>
2023-05-20 15:19:11 +0200

    clang

--- gitpodtest.sh
+++ gitpodtest.sh
@@ -4,6 +4,6 @@ mkdir ../.test/unit/hypercomplex
 cp Hypercomplex.hpp ../.test/unit/hypercomplex/Hypercomplex.hpp
 cp Polynomial.hpp ../.test/unit/hypercomplex/Polynomial.hpp
 cd ../.test/unit
-g++ -O0 -Wall --std=c++17 -o test test.cpp -lmpfr -lgmp -fconstexpr-steps=4294967295
+clang++ -O0 -Wall --std=c++17 -o test test.cpp -lmpfr -lgmp -fconstexpr-steps=4294967295
 ./test ~[local] -d yes -w NoAssertions --use-colour yes --benchmark-samples 100 --benchmark-resamples 100000
 rm -rf hypercomplex test
