e58d8008bf3b73cfa3bcfcea14bc51e59415a7d6
AngryMaciek <AngryMaciek@users.noreply.github.com>
2023-05-23 13:33:24 +0000

    deploy: a4a0c7670e98e8a5bdac148efe660963f3f9c6e0

--- docs/html/search/functions_6.js
+++ docs/html/search/functions_6.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['im_0',['Im',['../_hypercomplex_8hpp.html#ab232dad8df7b0f66c9cc201bca82f9a5',1,'Im(const Hypercomplex&lt; T, dim &gt; &amp;H):&#160;Hypercomplex.hpp'],['../_hypercomplex_8hpp.html#ab4e6a3251ab8dd6894d34b717c2c997e',1,'Im(const Hypercomplex&lt; mpfr_t, dim &gt; &amp;H):&#160;Hypercomplex.hpp'],['../_hypercomplex_8hpp.html#ae07b855ec5c6944b87c6ff2b63295677',1,'Im(const Hypercomplex&lt; Polynomial&lt; MaxDeg &gt;, dim &gt; &amp;H):&#160;Hypercomplex.hpp']]],
-  ['inv_1',['inv',['../class_hypercomplex.html#adb578e6896d59943d9168928009fe562',1,'Hypercomplex::inv()'],['../class_hypercomplex_3_01mpfr__t_00_01dim_01_4.html#a463b637e17efc7b910e5bc8638d9d0aa',1,'Hypercomplex&lt; mpfr_t, dim &gt;::inv()']]]
+  ['init_1',['init',['../class_hypercomplex.html#a15291cd83909b370a2cc9b7af07332a2',1,'Hypercomplex::init()'],['../class_hypercomplex_3_01mpfr__t_00_01dim_01_4.html#ad13e2c8f4b662670e6f0b218cb8b83ed',1,'Hypercomplex&lt; mpfr_t, dim &gt;::init()'],['../class_hypercomplex_3_01_polynomial_3_01_max_deg_01_4_00_01dim_01_4.html#a082192bdd1ae6714a8d04dc955306c43',1,'Hypercomplex&lt; Polynomial&lt; MaxDeg &gt;, dim &gt;::init()']]],
+  ['inv_2',['inv',['../class_hypercomplex.html#adb578e6896d59943d9168928009fe562',1,'Hypercomplex::inv()'],['../class_hypercomplex_3_01mpfr__t_00_01dim_01_4.html#a463b637e17efc7b910e5bc8638d9d0aa',1,'Hypercomplex&lt; mpfr_t, dim &gt;::inv()']]]
 ];
